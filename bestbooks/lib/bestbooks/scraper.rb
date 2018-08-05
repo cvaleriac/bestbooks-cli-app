59758df9ada1bb5a94c7e4780d7f1b3b3dae2f78
valeria campilongo <cvaleriac@hotmail.it>
2018-08-05 22:16:31 +0000

    scraper

--- bestbooks/lib/bestbooks/scraper.rb
+++ bestbooks/lib/bestbooks/scraper.rb
@@ -3,23 +3,19 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-   @@doc = Nokogiri::HTML(open("https://www.mondadoristore.it/Best-Seller-libri/gr-308/"))
 
    def self.scrape_books
-    titles = @@doc.search("cla-title").text.split("\n").compact.reject { |i| i.empty? }.map { |i| i[0...-13] }
 
-    authors = @@doc.search("secondary-data").text.split(" by ").reject { |i| i.empty?}
+     def scrape_books
+   doc = Nokogiri::HTML(open("https://www.mondadoristore.it/Best-Seller-libri/gr-308/"))
 
-    urls = @@doc.css("link").map { |i| i['href'].split(";")[0] }
+   doc.css(".product-info").each do |product|
+      book = Book.new
+      book.title = product-info.css("h3").text
+      book.title = product-info.css(".title").text
 
-    descriptions = urls.map { |ind_url|
-      url = ["https://www.mondadoristore.it",ind_url].join()
-      Nokogiri::HTML(open(url)).css('div#product-descriptions').text.split("\n").compact.reject { |i| i.empty? }[0] }
-
-    [*0..9].map! { |index|
-      Bestbooks::Book.new(attributes = {title: titles[index], author: authors[index], url: urls[index], description: descriptions[index]}) }
+    end
+  end
   end
 
-end
-binding.pry
 Scraper.new.scrape_books
