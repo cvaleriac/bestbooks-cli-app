b5c741063f4aecfc5261118f962a942112236559
valeria campilongo <cvaleriac@hotmail.it>
2018-08-05 22:27:41 +0000

    scraper changes

--- bestbooks/lib/bestbooks/scraper.rb
+++ bestbooks/lib/bestbooks/scraper.rb
@@ -10,9 +10,9 @@ class Scraper
    doc = Nokogiri::HTML(open("https://www.mondadoristore.it/Best-Seller-libri/gr-308/"))
 
    doc.css(".product-info").each do |product|
-      book = Book.new
       book.title = product-info.css("h3").text
       book.title = product-info.css(".title").text
+      book.description = product-description.css(".text").text
 
     end
   end
