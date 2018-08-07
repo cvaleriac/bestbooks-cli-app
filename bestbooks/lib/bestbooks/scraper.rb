598e3a332fc6b498ccf9f58db7f58df8cc337514
valeria campilongo <cvaleriac@hotmail.it>
2018-08-07 20:56:23 +0000

    removing pry

--- bestbooks/lib/bestbooks/scraper.rb
+++ bestbooks/lib/bestbooks/scraper.rb
@@ -8,11 +8,10 @@ class Scraper
 
    doc = Nokogiri::HTML(open("https://www.mondadoristore.it/Best-Seller-libri/gr-308/"))
 
-   doc.css(".product-info").each do |product|
-     binding.pry
-      book.title = product-info.css("h3").text
-      book.title = product-info.css(".title").text
-      book.description = product-description.css(".text").text
+      doc.css(".product-info").each do |product|
+      book.title = product.css("h3").text
+      book.title = product.css(".title").text
+      #book.description = product-description.css(".text").text
 
     end
   end
